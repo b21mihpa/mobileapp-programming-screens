45706091295f9e564dc71db62c384b4fb88e9f71
b21mihpa <b21mihpa@student.his.se>
2022-04-13 15:27:45 +0200

    added: Button that launches SecondActivity

--- app/src/main/java/com/example/screens/MainActivity.java
+++ app/src/main/java/com/example/screens/MainActivity.java
@@ -2,13 +2,22 @@ package com.example.screens;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        button = findViewById(R.id.button);
+        button.setOnClickListener(view -> {
+            Intent intent = new Intent(this, SecondActivity.class);
+            startActivity(intent);
+        });
     }
 }
