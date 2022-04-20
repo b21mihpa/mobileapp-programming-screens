70a11549c3c228828c9bd7ddeb6b6392ea6c676b
b21mihpa <b21mihpa@student.his.se>
2022-04-20 22:31:46 +0200

    added: TextView to SecondActivity and retrieved its contents from intent extras

--- app/src/main/java/com/example/screens/MainActivity.java
+++ app/src/main/java/com/example/screens/MainActivity.java
@@ -5,18 +5,22 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
-    private Button button;
+    private EditText input;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        button = findViewById(R.id.button);
+        input = findViewById(R.id.input);
+        Button button = findViewById(R.id.button);
+
         button.setOnClickListener(view -> {
             Intent intent = new Intent(this, SecondActivity.class);
+            intent.putExtra("message", input.getText().toString());
             startActivity(intent);
         });
     }
--- app/src/main/java/com/example/screens/SecondActivity.java
+++ app/src/main/java/com/example/screens/SecondActivity.java
@@ -3,6 +3,7 @@ package com.example.screens;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class SecondActivity extends AppCompatActivity {
 
@@ -10,5 +11,12 @@ public class SecondActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+
+        TextView message = findViewById(R.id.message);
+        message.setText(capitalize(getIntent().getStringExtra("message")));
+    }
+
+    private String capitalize(String string) {
+        return string.substring(0, 1).toUpperCase() + string.substring(1);
     }
 }
